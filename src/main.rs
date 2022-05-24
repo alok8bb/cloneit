b082ea079791071c43f586842a109eabc12cc3d2
winterrdog <winterrdog@keemail.me>
2022-05-24 07:39:07 +0300

    added zip archiving feature, refactored the code to use `borrow`ing more, capability to download content from more than 1 GitHub URL and a new form of argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,76 @@
-use clap::{load_yaml, App};
+#![warn(clippy::pedantic, clippy::all)]
 use colored::Colorize;
 use console::style;
 use std::process;
 
+pub mod arg_parser;
+pub mod file_archiver;
 pub mod output;
 pub mod parser;
 pub mod requests;
 
+use crate::arg_parser::create_args_parser;
+use crate::file_archiver::ZipArchiver;
+
 #[tokio::main]
 async fn main() {
-    let yaml = load_yaml!("cli.yaml");
-    let matches = App::from(yaml).get_matches();
+    let arg_vec_handle = create_args_parser();
+    let matches = arg_vec_handle.get_matches();
 
-    let url = matches.value_of("URL");
+    let urls = matches.values_of("url").unwrap().collect::<Vec<&str>>();
+    for url in &urls {
+        println!(
+            "
+            Getting: {:?}",
+            url
+        );
+        println!(
+            "{} {}Validating url...",
+            style("[1/3]").bold().dim(),
+            output::LOOKING_GLASS
+        );
+
+        let path = match parser::parse_url(url) {
+            Ok(path) => path,
+            Err(err) => {
+                eprintln!("{}", err.to_string().red());
+                process::exit(0);
+            }
+        };
+
+        let data = match parser::parse_path(&path) {
+            Ok(data) => data,
+            Err(err) => {
+                eprintln!("{}", err.to_string().red());
+                process::exit(0);
+            }
+        };
+
+        println!(
+            "{} {}Downloading...",
+            style("[2/3]").bold().dim(),
+            output::TRUCK
+        );
+
+        match requests::fetch_data(&data).await {
+            Err(err) => eprintln!("{}", err.to_string().red()),
+            Ok(_) => println!(
+                "{} {}Downloaded Successfully.",
+                style("[3/3]").bold().dim(),
+                output::SPARKLES
+            ),
+        };
+
+        if matches.is_present("zip") {
+            let dst_zip = format!("{}.zip", &data.root);
+            let zipper = ZipArchiver::new(&data.root, &dst_zip);
+            zipper.run().unwrap();
+        }
+    }
 
     println!(
-        "{} {}Validating url...",
-        style("[1/3]").bold().dim(),
-        output::LOOKING_GLASS
+        "
+        [+] Downloaded {:?} file(s).",
+        &urls.len()
     );
-    let path = match parser::parse_url(url.unwrap()) {
-        Ok(path) => path,
-        Err(err) => {
-            eprintln!("{}", err.to_string().red());
-            process::exit(0);
-        }
-    };
-
-    let data = match parser::parse_path(&path) {
-        Ok(data) => data,
-        Err(err) => {
-            eprintln!("{}", err.to_string().red());
-            process::exit(0);
-        }
-    };
-
-    println!(
-        "{} {}Downloading...",
-        style("[2/3]").bold().dim(),
-        output::TRUCK
-    );
-
-    match requests::fetch_data(data).await {
-        Err(err) => eprintln!("{}", err.to_string().red()),
-        Ok(_) => println!(
-            "{} {}Downloaded Successfully.",
-            style("[3/3]").bold().dim(),
-            output::SPARKLES
-        ),
-    };
 }
