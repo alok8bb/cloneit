cfdd34579961e38019ef9a6583a2007a59fbc499
alok8bb <alok8bb@gmail.com>
2022-08-14 09:36:10 +0530

    fix: remove `content_write` as sometimes its empty

--- src/requests.rs
+++ src/requests.rs
@@ -19,10 +19,7 @@ enum ApiResponse {
 pub struct ApiObject {
     name: String,
     path: String,
-    sha: String,
-    size: i64,
     url: String,
-    content: Option<String>,
     download_url: Option<String>,
     #[serde(rename = "type")]
     object_type: String, // dir or file
@@ -99,7 +96,6 @@ async fn download(url: &str, project_root: &str) -> Result<(), Box<dyn Error>> {
 
 #[async_recursion]
 async fn get_dir(url: &str, client: &Client, path: &Path) -> Result<(), Box<dyn Error>> {
-    println!("{:#?}", path);
     let resp = build_request(url, client).await?;
 
     match resp {
@@ -127,20 +123,13 @@ async fn write_file(
     root_path: &Path,
     client: &Client,
 ) -> Result<(), Box<dyn Error>> {
-    match obj.content {
-        Some(c) => {
-            let content_bytes =
-                base64::decode(c.chars().filter(|c| !c.is_whitespace()).collect::<String>())?;
+    println!("downloading: {}", obj.name);
 
-            tokio::fs::File::create(root_path.join(obj.name))
-                .await?
-                .write_all(&content_bytes[..])
-                .await?;
-        }
-        None => {
+    match &obj.download_url {
+        Some(download_url) => {
             let mut outfile = tokio::fs::File::create(root_path.join(obj.name)).await?;
 
-            let mut res = client.get(&obj.download_url.unwrap()).send().await?;
+            let mut res = client.get(download_url).send().await?;
             if !res.status().is_success() {
                 return Err(
                     format!("Couldn't download file from URL\nError: {}", res.status()).into(),
@@ -150,8 +139,8 @@ async fn write_file(
             while let Some(chunk) = res.chunk().await? {
                 outfile.write(&chunk).await?;
             }
+            Ok(())
         }
+        None => return Err(format!("Could not get the download link!").into()),
     }
-
-    Ok(())
 }
