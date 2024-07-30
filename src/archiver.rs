34327888234f22721a3e563758ef19b93a871029
uncenter <47499684+uncenter@users.noreply.github.com>
2024-07-29 21:02:59 -0400

    refactor: zip archiver craziness

--- src/archiver.rs
+++ src/archiver.rs
@@ -1,109 +1,88 @@
+use color_eyre::eyre::Result;
 use std::{
-    cell::Cell,
     fs::File,
-    io::{Read, Seek, Write},
+    io::{Read, Write},
     iter::Iterator,
     path::Path,
 };
-use zip::{result::ZipError, write::FileOptions};
 
-use walkdir::{DirEntry, WalkDir};
+use walkdir::WalkDir;
+use zip::{write::FileOptions, ZipWriter};
 
-#[derive(Debug)]
-pub struct ZipArchiver {
-    m_src_dir: String,
-    m_dest_zip_fname: String,
-    m_dst_zip_exists: Cell<bool>, // interior mutability
-    m_compress_method: zip::CompressionMethod,
+pub struct ZipArchiver<'a> {
+    src: &'a Path,
+    dest: &'a Path,
+    options: FileOptions,
 }
 
-impl ZipArchiver {
+impl<'a> ZipArchiver<'a> {
     #[must_use]
-    pub fn new(src_dir: &str, dest_zip_fname: &str) -> Self {
+    pub fn new(src: &'a str, dest: &'a str) -> Self {
+        let options = FileOptions::default()
+            .compression_method(zip::CompressionMethod::Deflated)
+            .unix_permissions(0o755);
+
         Self {
-            m_src_dir: src_dir.to_string(),
-            m_dest_zip_fname: dest_zip_fname.to_string(),
-            m_dst_zip_exists: Cell::new(false),
-            m_compress_method: zip::CompressionMethod::Deflated,
+            src: Path::new(src),
+            dest: Path::new(dest),
+            options,
         }
     }
 
-    fn zip_dir<T>(
-        &self,
-        dir_entries: &mut dyn Iterator<Item = DirEntry>,
-        prefix: &str,
-        writer: T,
-    ) -> zip::result::ZipResult<()>
-    where
-        T: Write + Seek + Read,
-    {
-        let mut zip = if self.m_dst_zip_exists.get() {
-            zip::ZipWriter::new_append(writer).unwrap()
+    fn add_path(&self, zip: &mut ZipWriter<File>, path: &Path) -> Result<()> {
+        let name = path.strip_prefix(self.src)?.to_str().unwrap();
+
+        // Write file or directory explicitly
+        // Some unzip tools unzip files with directory paths correctly, some do not!
+        if path.is_file() {
+            log::info!("[+] Adding file {:?} as {:?}...", path, name);
+            zip.start_file(name, self.options)?;
+
+            let mut file = File::open(path)?;
+            let mut buff = Vec::new();
+            file.read_to_end(&mut buff)?;
+            zip.write_all(&*buff)?;
+            buff.clear();
+        } else if !name.is_empty() {
+            // Only if not root! Avoids path spec / warning
+            // and mapname conversion failed error on unzip
+            log::info!("[+] Adding dir {:?} as {:?}...", path, name);
+            zip.add_directory(name, self.options)?;
+        }
+
+        Ok(())
+    }
+
+    pub fn run(&self) -> Result<()> {
+        // Individually downloaded files are not placed in the `src` directory,
+        // and so their paths are unknown and cannot be added to the zip.
+        if !self.src.is_dir() {
+            log::warn!("Unable to zip individual files, skipping");
+            return Ok(());
+        }
+
+        let dest = self.dest;
+        let dest_exists = dest.exists();
+
+        let writer = File::options()
+            .read(true)
+            .write(true)
+            .create(true)
+            .open(&dest)?;
+
+        let mut zip = if dest_exists {
+            zip::ZipWriter::new_append(writer)?
         } else {
             zip::ZipWriter::new(writer)
         };
 
-        let options = FileOptions::default()
-            .compression_method(self.m_compress_method)
-            .unix_permissions(0o755);
-
-        let mut file_buf = Vec::new();
-        for curr_entry in dir_entries {
-            let entry_path = curr_entry.path();
-            let entry_name = entry_path
-                .strip_prefix(Path::new(prefix))
-                .unwrap()
-                .to_str()
-                .unwrap();
-
-            // Write file or directory explicitly
-            // Some unzip tools unzip files with directory paths correctly, some do not!
-            if entry_path.is_file() {
-                log::info!("[+] Adding file {:?} as {:?}...", entry_path, entry_name);
-                zip.start_file(entry_name, options)?;
-                let mut fh = File::open(entry_path)?;
-
-                fh.read_to_end(&mut file_buf)?;
-                zip.write_all(&*file_buf)?;
-                file_buf.clear();
-            } else if !entry_name.is_empty() {
-                // Only if not root! Avoids path spec / warning
-                // and mapname conversion failed error on unzip
-                log::info!("[+] Adding dir {:?} as {:?}...", entry_path, entry_name);
-                zip.add_directory(entry_name, options)?;
-            }
+        for entry in WalkDir::new(&self.src).into_iter().filter_map(Result::ok) {
+            self.add_path(&mut zip, entry.path())?;
         }
 
         zip.finish()?;
-        Result::Ok(())
-    }
 
-    pub fn run(&self) -> zip::result::ZipResult<()> {
-        if !Path::new(&self.m_src_dir).is_dir() {
-            return Err(ZipError::FileNotFound);
-        }
-
-        let path = Path::new(&self.m_dest_zip_fname);
-
-        if path.exists() {
-            self.m_dst_zip_exists.set(true);
-        }
-
-        let file = File::options()
-            .read(true)
-            .write(true)
-            .create(true)
-            .open(&path)?;
-
-        let dir_content = WalkDir::new(&self.m_src_dir).into_iter();
-
-        self.zip_dir(
-            &mut dir_content.filter_map(Result::ok),
-            &self.m_src_dir,
-            file,
-        )?;
-
-        log::info!("[+] Zip archived as {:?}.", &self.m_dest_zip_fname);
+        log::info!("[+] Zip archived as {:?}.", &self.dest);
 
         Ok(())
     }
